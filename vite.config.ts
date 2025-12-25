9b92d40bf34724ca4abb8e9069f235f537a5e3ae
dddppp-de <8davidp@gmail.com>
2025-12-25 18:40:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // GitHub Pages base URL
-  base: "/xixix/",
+  base: "/akku-recycling-direkt/",
   server: {
     host: "::",
     port: 8080,
